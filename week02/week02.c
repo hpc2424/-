38632c34932d669f1bb0d09fa1eda8f4a86b16e0
hpc2424 <2zerobad@gmail.com>
2024-09-11 16:17:03 +0900

    summation

--- week02/week02.c
+++ week02/week02.c
@@ -4,6 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello Sookmyung Electronics!");
+	int a;
+	int b;
+	int sum;
+	a = 10;
+	b = 20;
+	sum = a + b;
+	printf("Hello Sookmyung Electronic\n");
+	printf("%d + %d = %d\n", a, b, a+b);
+	
 	return 0;
 }
