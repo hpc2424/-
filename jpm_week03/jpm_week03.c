7aee66123e8f70c2d738c4b7c8864415f0c96ed7
hpc2424 <2zerobad@gmail.com>
2024-11-06 13:20:58 +0900

    .

--- jpm_week03/jpm_week03.c
+++ jpm_week03/jpm_week03.c
@@ -4,16 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x, y, z, m;
-	int a, b, c;
-	x = 2;
-	z = 1;
-	a = 3;
-	b = 4;
-	c = 5;
-	y = a*x^2 + b*x + c;
-	m = (x + y + z)/3;
-	printf("y=%d, m=%d", y, m);
-	
+	printf("hello world");
 	return 0;
 }
